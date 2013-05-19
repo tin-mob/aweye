0a9be1872de50356d0887a79bbe1b2cc99a61292
Robert Morin <tin.mob.error@gmail.com>
2013-05-19 02:47:51 -0400

    - Removed news from testEWPresenter.

--- TestEyeWatcher/src/testEWPresenter.cpp
+++ TestEyeWatcher/src/testEWPresenter.cpp
@@ -12,59 +12,31 @@
 struct EWPresenterFixture
 {
     public:
-        EWPresenterFixture() : msgHandler(NULL), keeper(NULL),
-            checkTimer(NULL), clockTimer(NULL), presenter(NULL)
+        EWPresenterFixture() :
+            data({boost::posix_time::seconds(5), boost::posix_time::seconds(3),
+                boost::posix_time::seconds(1), boost::posix_time::seconds(2)}),
+            msgHandler(), keeper(), checkTimer(), clockTimer(), dialog(), viewObserver(),
+            presenter(EWPresenter(&msgHandler, &keeper, &checkTimer,
+                &clockTimer, data.popupAlarm, true, data.soundPath))
         {
-            this->data = {boost::posix_time::seconds(5), boost::posix_time::seconds(3),
-                boost::posix_time::seconds(1), boost::posix_time::seconds(2)};
             data.soundAlarm = true;
-            try
-            {
-                this->msgHandler = new MsgHandlerStub();
-                this->keeper = new TimeKeeperStub();
-                this->checkTimer = new TimerStub();
-                this->clockTimer = new TimerStub();
-                this->dialog = new OptionsDialogStub();
-                this->viewObserver = new EWViewObserverStub();
-                this->presenter = new EWPresenter(this->msgHandler,
-                    this->keeper, this->checkTimer, this->clockTimer,
-                    data.popupAlarm, data.soundAlarm, data.soundPath);
-
-                this->presenter->attach(this->viewObserver);
-            }
-            catch (...)
-            {
-                this->deleteFixture();
-                throw;
-            }
+            presenter.attach(&viewObserver);
         }
         ~EWPresenterFixture()
         {
-            this->deleteFixture();
         }
 
         ConfigData data;
-        MsgHandlerStub* msgHandler;
-        TimeKeeperStub* keeper;
-        TimerStub* checkTimer;
-        TimerStub* clockTimer;
-        OptionsDialogStub* dialog;
-        EWViewObserverStub* viewObserver;
-
-        EWPresenter* presenter;
+        MsgHandlerStub msgHandler;
+        TimeKeeperStub keeper;
+        TimerStub checkTimer;
+        TimerStub clockTimer;
+        OptionsDialogStub dialog;
+        EWViewObserverStub viewObserver;
+        EWPresenter presenter;
 
     protected:
     private:
-        void deleteFixture()
-        {
-            if (this->msgHandler != NULL) {delete this->msgHandler;}
-            if (this->keeper != NULL) {delete this->keeper;}
-            if (this->checkTimer != NULL) {delete this->checkTimer;}
-            if (this->clockTimer != NULL) {delete this->clockTimer;}
-            if (this->dialog != NULL) {delete this->dialog;}
-            if (this->viewObserver != NULL) {delete this->viewObserver;}
-            if (this->presenter != NULL) {delete this->presenter;}
-        }
 };
 
 SUITE(TestEWPresenter)
@@ -90,8 +62,8 @@ SUITE(TestEWPresenter)
             "test.wav"
         };
 
-        CHECK_EQUAL(true, presenter->saveConfig(newData));
-        ConfigData returnedData = presenter->getConfigData();
+        CHECK_EQUAL(true, this->presenter.saveConfig(newData));
+        ConfigData returnedData = this->presenter.getConfigData();
 
         CHECK_EQUAL(newData.workLength, returnedData.workLength);
         CHECK_EQUAL(newData.pauseLength, returnedData.pauseLength);
@@ -112,140 +84,140 @@ SUITE(TestEWPresenter)
         CHECK_EQUAL(newData.faceSizeX, this->presHandler->faceSizeX);
         CHECK_EQUAL(newData.faceSizeY, this->presHandler->faceSizeY);
 
-        CHECK_EQUAL(newData.checkFreq, this->keeper->checkFreq);
-        CHECK_EQUAL(newData.pauseLength, this->keeper->pauseLength);
-        CHECK_EQUAL(newData.pauseTol, this->keeper->pauseTol);
-        CHECK_EQUAL(newData.remFreq, this->keeper->remFreq);
-        CHECK_EQUAL(newData.workLength, this->keeper->workLength);
+        CHECK_EQUAL(newData.checkFreq, this->keeper.checkFreq);
+        CHECK_EQUAL(newData.pauseLength, this->keeper.pauseLength);
+        CHECK_EQUAL(newData.pauseTol, this->keeper.pauseTol);
+        CHECK_EQUAL(newData.remFreq, this->keeper.remFreq);
+        CHECK_EQUAL(newData.workLength, this->keeper.workLength);
     }
 
     TEST_FIXTURE(EWPresenterFixture, TestInvalidConfig)
     {
             config->fail = true;
-            CHECK_EQUAL(false, presenter->saveConfig(data));
-            CHECK_EQUAL("Testing!", msgHandler->lastError);
+            CHECK_EQUAL(false, this->presenter.saveConfig(data));
+            CHECK_EQUAL("Testing!", this->msgHandler.lastError);
     }*/
 
     TEST_FIXTURE(EWPresenterFixture, TestStartStop)
     {
-        presenter->toggleStart();
-        CHECK_EQUAL(keeper->getStatus(), AbstractTimeKeeper::HERE);
-        CHECK_EQUAL(checkTimer->running, true);
-        CHECK_EQUAL(clockTimer->running, true);
-        CHECK_EQUAL(viewObserver->checkStatUpdated(), true);
+        this->presenter.toggleStart();
+        CHECK_EQUAL(this->keeper.getStatus(), AbstractTimeKeeper::HERE);
+        CHECK_EQUAL(this->checkTimer.running, true);
+        CHECK_EQUAL(this->clockTimer.running, true);
+        CHECK_EQUAL(this->viewObserver.checkStatUpdated(), true);
 
-        presenter->toggleStart();
-        CHECK_EQUAL(keeper->getStatus(), AbstractTimeKeeper::OFF);
-        CHECK_EQUAL(checkTimer->running, false);
-        CHECK_EQUAL(clockTimer->running, false);
-        CHECK_EQUAL(viewObserver->checkStatUpdated(), true);
+        this->presenter.toggleStart();
+        CHECK_EQUAL(this->keeper.getStatus(), AbstractTimeKeeper::OFF);
+        CHECK_EQUAL(this->checkTimer.running, false);
+        CHECK_EQUAL(this->clockTimer.running, false);
+        CHECK_EQUAL(this->viewObserver.checkStatUpdated(), true);
 
-        keeper->fail = true;
-        CHECK_EQUAL(msgHandler->lastError, "");
-        presenter->toggleStart();
-        CHECK_EQUAL(keeper->getStatus(), AbstractTimeKeeper::OFF);
-        CHECK_EQUAL(checkTimer->running, false);
-        CHECK_EQUAL(clockTimer->running, false);
-        CHECK_EQUAL(viewObserver->checkStatUpdated(), false);
-        CHECK_EQUAL(msgHandler->lastError, "Testing!");
+        this->keeper.fail = true;
+        CHECK_EQUAL(this->msgHandler.lastError, "");
+        this->presenter.toggleStart();
+        CHECK_EQUAL(this->keeper.getStatus(), AbstractTimeKeeper::OFF);
+        CHECK_EQUAL(this->checkTimer.running, false);
+        CHECK_EQUAL(this->clockTimer.running, false);
+        CHECK_EQUAL(this->viewObserver.checkStatUpdated(), false);
+        CHECK_EQUAL(this->msgHandler.lastError, "Testing!");
     }
 
     TEST_FIXTURE(EWPresenterFixture, TestUpdate)
     {
-        CHECK_EQUAL(presenter->getStatus(), "Off");
-        CHECK_EQUAL(presenter->getTimeOn(), "10:59:00");
-        CHECK_EQUAL(presenter->getTimeOff(), "11:31:01");
-        CHECK_EQUAL(presenter->getTimeRunning(), "00:00:02");
-        CHECK_EQUAL(presenter->getTimeLeft(), "00:03:00");
-        CHECK_EQUAL(msgHandler->lastAlert, "");
+        CHECK_EQUAL(this->presenter.getStatus(), "Off");
+        CHECK_EQUAL(this->presenter.getTimeOn(), "10:59:00");
+        CHECK_EQUAL(this->presenter.getTimeOff(), "11:31:01");
+        CHECK_EQUAL(this->presenter.getTimeRunning(), "00:00:02");
+        CHECK_EQUAL(this->presenter.getTimeLeft(), "00:03:00");
+        CHECK_EQUAL(this->msgHandler.lastAlert, "");
 
-        presenter->toggleStart();
-        keeper->hereStamp = boost::posix_time::not_a_date_time;
-        keeper->awayStamp = boost::posix_time::not_a_date_time;
-        keeper->interval = boost::posix_time::not_a_date_time;
-        keeper->left = boost::posix_time::not_a_date_time;
+        this->presenter.toggleStart();
+        this->keeper.hereStamp = boost::posix_time::not_a_date_time;
+        this->keeper.awayStamp = boost::posix_time::not_a_date_time;
+        this->keeper.interval = boost::posix_time::not_a_date_time;
+        this->keeper.left = boost::posix_time::not_a_date_time;
 
-        CHECK_EQUAL(presenter->getStatus(), "Here");
-        CHECK_EQUAL(presenter->getTimeOn(), "00:00:00");
-        CHECK_EQUAL(presenter->getTimeOff(), "00:00:00");
-        CHECK_EQUAL(presenter->getTimeRunning(), "00:00:00");
-        CHECK_EQUAL(presenter->getTimeLeft(), "00:00:00");
-        CHECK_EQUAL(msgHandler->lastAlert, "");
-        CHECK_EQUAL(msgHandler->lastSound, "");
+        CHECK_EQUAL(this->presenter.getStatus(), "Here");
+        CHECK_EQUAL(this->presenter.getTimeOn(), "00:00:00");
+        CHECK_EQUAL(this->presenter.getTimeOff(), "00:00:00");
+        CHECK_EQUAL(this->presenter.getTimeRunning(), "00:00:00");
+        CHECK_EQUAL(this->presenter.getTimeLeft(), "00:00:00");
+        CHECK_EQUAL(this->msgHandler.lastAlert, "");
+        CHECK_EQUAL(this->msgHandler.lastSound, "");
 
-        keeper->late = true;
-        presenter->togglePause();
-        presenter->update(this->checkTimer);
-        CHECK_EQUAL(msgHandler->lastAlert, "");
-        CHECK_EQUAL(msgHandler->lastSound, "");
+        this->keeper.late = true;
+        this->presenter.togglePause();
+        this->presenter.update(&this->checkTimer);
+        CHECK_EQUAL(this->msgHandler.lastAlert, "");
+        CHECK_EQUAL(this->msgHandler.lastSound, "");
 
-        presenter->togglePause();
-        presenter->update(this->checkTimer);
-        CHECK_EQUAL(msgHandler->lastAlert, presenter->m_LateMsg);
-        CHECK_EQUAL(msgHandler->lastSound, data.soundPath);
+        this->presenter.togglePause();
+        this->presenter.update(&this->checkTimer);
+        CHECK_EQUAL(this->msgHandler.lastAlert, this->presenter.m_LateMsg);
+        CHECK_EQUAL(this->msgHandler.lastSound, data.soundPath);
 
-        keeper->fail = true;
-        CHECK_EQUAL(msgHandler->lastError, "");
-        presenter->update(this->checkTimer);
-        CHECK_EQUAL(msgHandler->lastError, "Testing!");
+        this->keeper.fail = true;
+        CHECK_EQUAL(this->msgHandler.lastError, "");
+        this->presenter.update(&this->checkTimer);
+        CHECK_EQUAL(this->msgHandler.lastError, "Testing!");
     }
 
     TEST_FIXTURE(EWPresenterFixture, TestQuit)
     {
-        presenter->quit();
-        CHECK_EQUAL(viewObserver->checkQuitUpdated(), true);
+        this->presenter.quit();
+        CHECK_EQUAL(this->viewObserver.checkQuitUpdated(), true);
     }
 
     TEST_FIXTURE(EWPresenterFixture, TestPauseButtonsLabels)
     {
-        CHECK_EQUAL(presenter->getPauseButtonLabel(), presenter->m_PauseBtnLabel);
-        presenter->togglePause();
-        CHECK_EQUAL(presenter->getPauseButtonLabel(), presenter->m_ResumeBtnLabel);
+        CHECK_EQUAL(this->presenter.getPauseButtonLabel(), this->presenter.m_PauseBtnLabel);
+        this->presenter.togglePause();
+        CHECK_EQUAL(this->presenter.getPauseButtonLabel(), this->presenter.m_ResumeBtnLabel);
     }
 
     TEST_FIXTURE(EWPresenterFixture, TestStartButtonsLabels)
     {
-        CHECK_EQUAL(presenter->getStartButtonLabel(), presenter->m_StartBtnLabel);
-        presenter->toggleStart();
-        CHECK_EQUAL(presenter->getStartButtonLabel(), presenter->m_StopBtnLabel);
+        CHECK_EQUAL(this->presenter.getStartButtonLabel(), this->presenter.m_StartBtnLabel);
+        this->presenter.toggleStart();
+        CHECK_EQUAL(this->presenter.getStartButtonLabel(), this->presenter.m_StopBtnLabel);
     }
 
     TEST_FIXTURE(EWPresenterFixture, TestHideButtonsLabels)
     {
-        CHECK_EQUAL(presenter->getHideButtonLabel(), presenter->m_HideBtnLabel);
-        CHECK_EQUAL(true, presenter->isShown());
+        CHECK_EQUAL(this->presenter.getHideButtonLabel(), this->presenter.m_HideBtnLabel);
+        CHECK_EQUAL(true, this->presenter.isShown());
 
-        presenter->show(false);
-        CHECK_EQUAL(presenter->getHideButtonLabel(), presenter->m_RestoreBtnLabel);
-        CHECK_EQUAL(false, presenter->isShown());
+        this->presenter.show(false);
+        CHECK_EQUAL(this->presenter.getHideButtonLabel(), this->presenter.m_RestoreBtnLabel);
+        CHECK_EQUAL(false, this->presenter.isShown());
 
-        presenter->show(true);
-        CHECK_EQUAL(presenter->getHideButtonLabel(), presenter->m_HideBtnLabel);
-        CHECK_EQUAL(true, presenter->isShown());
+        this->presenter.show(true);
+        CHECK_EQUAL(this->presenter.getHideButtonLabel(), this->presenter.m_HideBtnLabel);
+        CHECK_EQUAL(true, this->presenter.isShown());
     }
 
     TEST_FIXTURE(EWPresenterFixture, TestIconName)
     {
-        CHECK_EQUAL(presenter->getIconName(), presenter->m_StopWebcamIcon);
+        CHECK_EQUAL(this->presenter.getIconName(), this->presenter.m_StopWebcamIcon);
 
-        presenter->toggleStart();
-        keeper->workLeft = boost::posix_time::hours(1);
-        CHECK_EQUAL(presenter->getIconName(), presenter->m_GreenWebcamIcon);
+        this->presenter.toggleStart();
+        this->keeper.workLeft = boost::posix_time::hours(1);
+        CHECK_EQUAL(this->presenter.getIconName(), this->presenter.m_GreenWebcamIcon);
 
-        keeper->workLeft = boost::posix_time::seconds(1);
-        CHECK_EQUAL(presenter->getIconName(), presenter->m_YellowWebcamIcon);
+        this->keeper.workLeft = boost::posix_time::seconds(1);
+        CHECK_EQUAL(this->presenter.getIconName(), this->presenter.m_YellowWebcamIcon);
 
-        keeper->workLeft = boost::posix_time::seconds(0);
-        CHECK_EQUAL(presenter->getIconName(), presenter->m_RedWebcamIcon);
+        this->keeper.workLeft = boost::posix_time::seconds(0);
+        CHECK_EQUAL(this->presenter.getIconName(), this->presenter.m_RedWebcamIcon);
 
-        keeper->status = AbstractTimeKeeper::AWAY;
-        keeper->left = boost::posix_time::seconds(0);
-        CHECK_EQUAL(presenter->getIconName(), presenter->m_GreenWebcamIcon);
+        this->keeper.status = AbstractTimeKeeper::AWAY;
+        this->keeper.left = boost::posix_time::seconds(0);
+        CHECK_EQUAL(this->presenter.getIconName(), this->presenter.m_GreenWebcamIcon);
     }
 
     TEST_FIXTURE(EWPresenterFixture, TestUpdateTime)
     {
-        presenter->update(this->clockTimer);
-        CHECK_EQUAL(viewObserver->checkTimeUpdated(), true);
+        this->presenter.update(&this->clockTimer);
+        CHECK_EQUAL(this->viewObserver.checkTimeUpdated(), true);
     }
 }
