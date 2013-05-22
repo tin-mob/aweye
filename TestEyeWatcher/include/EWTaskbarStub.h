7d587f7fec9fccb86a649a9cc443e0d55396dd9f
Robert Morin <tin.mob.error@gmail.com>
2013-05-22 11:31:16 -0400

    - Testing EWTaskBarPres

---
+++ TestEyeWatcher/include/EWTaskbarStub.h
@@ -0,0 +1,50 @@
+#ifndef EWTASKBARSTUB_H
+#define EWTASKBARSTUB_H
+
+#include "AbstractEWViewPres.h"
+#include "AbstractEWTaskbar.h"
+
+class EWTaskbarStub : public AbstractEWTaskbar
+{
+    public:
+        EWTaskbarStub(AbstractEWViewPres* p = NULL) : presenter(p){}
+        virtual ~EWTaskbarStub() {}
+
+        virtual void setPopupMenuCommands( std::string hrl,
+            std::string ssl, std::string prl)
+        {
+            hideRestoreLabel = hrl;
+            startStopLabel = ssl;
+            pauseResumeLabel = prl;
+        }
+
+        virtual void setPopupMenuTimes( std::string onc, std::string offc,
+            std::string rc, std::string lc)
+        {
+            onClock = onc;
+            offClock = offc;
+            runningClock = rc;
+            leftClock = lc;
+        }
+
+        virtual void setIcon(std::string l)
+        {
+            loc = l;
+        }
+
+        std::string hideRestoreLabel;
+        std::string startStopLabel;
+        std::string pauseResumeLabel;
+        std::string onClock;
+        std::string offClock;
+        std::string runningClock;
+        std::string leftClock;
+        std::string loc;
+
+        AbstractEWViewPres* presenter;
+    protected:
+    private:
+};
+
+
+#endif // EWTASKBARSTUB_H
---
+++ TestEyeWatcher/src/testEWTaskBarPres.cpp
@@ -0,0 +1,120 @@
+#include <unittest++/UnitTest++.h>
+#include "EWTaskBarPres.h"
+#include "EWAppControllerStub.h"
+#include "EWPresenterStub.h"
+#include "EWTaskbarStub.h"
+
+///@todo: sort TaskBar/Taskbar
+struct EWTaskBarPresFixture
+{
+    EWTaskBarPresFixture() : ctrl(), pres(), barPres(&pres, &ctrl)
+    {
+        barPres.attachTaskBar(&bar);
+    }
+    ~EWTaskBarPresFixture() {}
+
+    EWAppControllerStub ctrl;
+    EWPresenterStub pres;
+    EWTaskBarPres barPres;
+    EWTaskbarStub bar;
+};
+
+SUITE(TestEWTaskBarPres)
+{
+    ///////////////////////////////////////////////////////////////////////////
+    // Specific
+    ///////////////////////////////////////////////////////////////////////////
+    TEST_FIXTURE(EWTaskBarPresFixture, TestTimesUpdate)
+    {
+        std::string hbl = "hbl";
+        std::string pbl = "pbl";
+        std::string sbl = "sbl";
+        std::string s = "s";
+        std::string ton = "ton";
+        std::string toff = "toff";
+        std::string tr = "tr";
+        std::string tl = "tl";
+        bool is = true;
+        std::string in = "in";
+
+        pres.setDisplayValues(hbl, pbl, sbl, s, ton, toff, tr, tl, is, in);
+        pres.notifyTime();
+
+        CHECK_EQUAL("Last Session : " + ton, bar.onClock);
+        CHECK_EQUAL("Last Pause : " + toff, bar.offClock);
+        CHECK_EQUAL("Running : " + tr, bar.runningClock);
+        CHECK_EQUAL("Time Left : " + tl, bar.leftClock);
+        CHECK_EQUAL(in, bar.loc);
+    }
+
+    TEST_FIXTURE(EWTaskBarPresFixture, TestTimesUpdateNoIcon)
+    {
+        std::string hbl = "hbl";
+        std::string pbl = "pbl";
+        std::string sbl = "sbl";
+        std::string s = "s";
+        std::string ton = "ton";
+        std::string toff = "toff";
+        std::string tr = "tr";
+        std::string tl = "tl";
+        bool is = true;
+        std::string in = "";
+
+        bar.loc = "in";
+
+        pres.setDisplayValues(hbl, pbl, sbl, s, ton, toff, tr, tl, is, in);
+        pres.notifyTime();
+
+        CHECK_EQUAL("Last Session : " + ton, bar.onClock);
+        CHECK_EQUAL("Last Pause : " + toff, bar.offClock);
+        CHECK_EQUAL("Running : " + tr, bar.runningClock);
+        CHECK_EQUAL("Time Left : " + tl, bar.leftClock);
+        CHECK_EQUAL("in", bar.loc);
+    }
+
+    TEST_FIXTURE(EWTaskBarPresFixture, TestStatusUpdate)
+    {
+        std::string hbl = "hbl";
+        std::string pbl = "pbl";
+        std::string sbl = "sbl";
+        std::string s = "s";
+        std::string ton = "ton";
+        std::string toff = "toff";
+        std::string tr = "tr";
+        std::string tl = "tl";
+        bool is = true;
+        std::string in = "in";
+
+        pres.setDisplayValues(hbl, pbl, sbl, s, ton, toff, tr, tl, is, in);
+        pres.notifyStatus();
+
+        CHECK_EQUAL(hbl, bar.hideRestoreLabel);
+        CHECK_EQUAL(sbl, bar.startStopLabel);
+        CHECK_EQUAL(pbl, bar.pauseResumeLabel);
+        CHECK_EQUAL(in, bar.loc);
+    }
+
+    TEST_FIXTURE(EWTaskBarPresFixture, TestStatusUpdateNoIcon)
+    {
+        std::string hbl = "hbl";
+        std::string pbl = "pbl";
+        std::string sbl = "sbl";
+        std::string s = "s";
+        std::string ton = "ton";
+        std::string toff = "toff";
+        std::string tr = "tr";
+        std::string tl = "tl";
+        bool is = true;
+        std::string in = "";
+
+        bar.loc = "in";
+
+        pres.setDisplayValues(hbl, pbl, sbl, s, ton, toff, tr, tl, is, in);
+        pres.notifyStatus();
+
+        CHECK_EQUAL(hbl, bar.hideRestoreLabel);
+        CHECK_EQUAL(sbl, bar.startStopLabel);
+        CHECK_EQUAL(pbl, bar.pauseResumeLabel);
+        CHECK_EQUAL("in", bar.loc);
+    }
+}
