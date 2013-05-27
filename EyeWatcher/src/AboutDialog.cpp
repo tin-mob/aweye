64c8fc2230c3f88dcdcc7f5a91ce9400e45ac75d
Robert Morin <tin.mob.error@gmail.com>
2013-05-27 17:22:44 -0400

    EyeWatcher to Eyes Watcher. Should be all right concerning the name (hopefully).

--- EyeWatcher/src/AboutDialog.cpp
+++ EyeWatcher/src/AboutDialog.cpp
@@ -1,20 +1,20 @@
 /***************************************************************
     Copyright: 2013, Robert Morin
 
-    This file is part of EyeWatcher.
+    This file is part of Eyes Watcher.
 
-    EyeWatcher is free software: you can redistribute it and/or modify
+    Eyes Watcher is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    EyeWatcher is distributed in the hope that it will be useful,
+    Eyes Watcher is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with EyeWatcher.  If not, see <http://www.gnu.org/licenses/>.
+    along with Eyes Watcher.  If not, see <http://www.gnu.org/licenses/>.
 
  **************************************************************/
 
@@ -49,7 +49,7 @@ AboutDialog::AboutDialog(wxWindow* parent,wxWindowID id,const wxPoint& pos,const
 
 	Create(parent, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE, _T("wxID_ANY"));
 	BoxSizer1 = new wxBoxSizer(wxVERTICAL);
-	StaticText1 = new wxStaticText(this, ID_STATICTEXT1, _("EyeWatcher - Watches your eyes and reminds you to take a break.\n\nCopyright (C) 2013 - Robert Morin\n\nThis program is free software: you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\n\nThis program is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\nGNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License\nalong with this program.  If not, see <http://www.gnu.org/licenses/>.\n\nwebcam.ico is part of WPZOOM Developer Icon Set by WPZOOM,\ndesigned by David Ferreira. http://www.wpzoom.com\nwebcam-*.png are derived from webcam.ico.\n\nThe icons are licensed under a Creative Commons Attribution-Share Alike 3.0\nUnported License (http://creativecommons.org/licenses/by-nc-sa/3.0/). \n\n"), wxDefaultPosition, wxDefaultSize, 0, _T("ID_STATICTEXT1"));
+	StaticText1 = new wxStaticText(this, ID_STATICTEXT1, _("Eyes Watcher - Watches your eyes and reminds you to take a break.\n\nCopyright (C) 2013 - Robert Morin\n\nThis program is free software: you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\n\nThis program is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\nGNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License\nalong with this program.  If not, see <http://www.gnu.org/licenses/>.\n\nwebcam.ico is part of WPZOOM Developer Icon Set by WPZOOM,\ndesigned by David Ferreira. http://www.wpzoom.com\nwebcam-*.png are derived from webcam.ico.\n\nThe icons are licensed under a Creative Commons Attribution-Share Alike 3.0\nUnported License (http://creativecommons.org/licenses/by-nc-sa/3.0/). \n\n"), wxDefaultPosition, wxDefaultSize, 0, _T("ID_STATICTEXT1"));
 	BoxSizer1->Add(StaticText1, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
 	StdDialogButtonSizer1 = new wxStdDialogButtonSizer();
 	StdDialogButtonSizer1->AddButton(new wxButton(this, wxID_OK, wxEmptyString));
